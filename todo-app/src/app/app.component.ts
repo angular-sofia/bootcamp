c46ec0e67f5957a68315e4db3f455c1bd967a769
vakrilov <alexander.vakrilov@telerik.com>
2019-02-02 16:29:57 +0200

    feat: list of todos and add-todo

--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Todo } from './todo.model';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-app';
+    newTodo = '';
+    todos: Todo[] = [
+        { name: 'learn angular' },
+        { name: 'create an app' }];
+
+    createTodo() {
+        this.todos.push({ name: this.newTodo });
+        this.newTodo = '';
+    }
 }
